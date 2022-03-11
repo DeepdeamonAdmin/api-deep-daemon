eecba0f316ff73fa8cef5c1d90b1c331f1f13946
DeepdeamonAdmin <101360955+DeepdeamonAdmin@users.noreply.github.com>
2022-03-11 11:20:05 -0600

    cargando api

---
+++ src/database.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql');
+
+// const mysqlConnection = mysql.createConnection({
+//     host : 'localhost',
+//     user : 'root',
+//     password : 'KOALA@96',
+//     database : 'deepdaem_web_admin'
+// });
+
+
+/*BD EN HOSTING*/
+const mysqlConnection = mysql.createConnection({
+    host : 'deepdaemon.org',
+    user : 'deepdae1_admin',
+    password : 'D33pD43monC1C2022',
+    database : 'deepdae1_adminweb'
+    
+});
+
+mysqlConnection.connect(function (err) {
+    if(err) {
+        console.log(err);
+    }else{
+        console.log('Conexion exitosa a la BD');
+    }  
+});
+
+//Para ver los errores en las sentencia Query
+mysqlConnection.on('error', function(err) {
+    console.log(err.code);
+  });
+
+module.exports = mysqlConnection;
+
